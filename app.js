8905c8876389364159dd34fa8a70c0a46d7f3eb9
Craig Mertens <craig@null.dom>
2021-07-09 15:17:34 -0500

    License and eslint

--- app.js
+++ app.js
@@ -1,26 +1,4 @@
 /************************************************************************************
-Copyright 2021 Craig Mertens
-
-Redistribution and use in source and binary forms, with or without modification, are permitted provided that 
-the following conditions are met:
-
-1. Redistributions of source code must retain the above copyright notice, this list of conditions and the 
-following disclaimer.
-
-2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and 
-the following disclaimer in the documentation and/or other materials provided with the distribution.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED 
-WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A 
-PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR 
-ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
-LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
-INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN 
-IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-************************************************************************************/
-
-/************************************************************************************
 
 This is a simple, single-file reverse proxy with basic capabilities for proxying and serving content.
 The proxy is SSL-secured and is capable of dispatching based on hostnames and URL paths, and supports 
@@ -106,7 +84,7 @@ var privateKey = fs.readFileSync(path.join(SSL_DIR, SSL_KEY_FILENAME));
 var certificate = fs.readFileSync(path.join(SSL_DIR, SSL_CERT_FILENAME));
 var tls = {key: privateKey, cert: certificate};
 
-// Track calls and drain each second
+// Track calls and drain DDOS protection counter each second
 var currentCalls = 0;
 setInterval(() => {
   currentCalls = (currentCalls - MAX_CALLS_PER_SECOND) < 0 ? 0 :  (currentCalls - MAX_CALLS_PER_SECOND);
